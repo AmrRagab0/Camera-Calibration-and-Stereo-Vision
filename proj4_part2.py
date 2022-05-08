219636bed4fc2b3169999bc678b062d8dabdda89
Abdullah-Eisa <63313373+Abdullah-Eisa@users.noreply.github.com>
2022-05-08 03:05:32 +0200

    Update proj4_part2.py

--- proj4_part2.py
+++ proj4_part2.py
@@ -1,11 +1,28 @@
-#  Load images and prepare them
+#  Load images and prepare them 
+img_r = cv2.imread('../data/pic_a.jpg')
+img_b = cv2.imread('../data/pic_b.jpg')
 
 
 
-#Extract 2d points and map them to 3d points
+# Extract 2d points and map them to 3d point 
 
+Points_2D = np.loadtxt('../data/pts2d-norm-pic_a.txt')
+Points_3D = np.loadtxt('../data/pts3d-norm.txt')
 
+##                              ????????????????????????????????????
+# Points_2D_r = 
+# up_right_3D = 
+# Points_3D_r = 
+# #Normalize
+# Points_2D_r = 
+# Points_3D_r = 
 
+# Points_2D_l = 
+# up_left_3D = 
+# Points_3D_l = 
+# #Normalize
+# Points_2D_l =
+# Points_3D_l =
 
 #Calculate the projection matrix given corresponding 2D and 3D points
 
@@ -33,3 +50,5 @@ Center_r = compute_camera_center(M_r)
 #compute the fundamental matrix
 
 # F=
+# F, mask = cv2.findFundamentalMat(img1_points, img2_points, cv2.FM_RANSAC)
+
