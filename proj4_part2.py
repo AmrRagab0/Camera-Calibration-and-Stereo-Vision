d4c2e08a2e5ed8edc69a03025bd37cd0d316fc4f
Abdullah-Eisa <63313373+Abdullah-Eisa@users.noreply.github.com>
2022-05-11 20:07:10 +0200

    Update proj4_part2.py

--- proj4_part2.py
+++ proj4_part2.py
@@ -13,7 +13,7 @@ Points_3D = np.loadtxt('../data/pts3d-norm.txt')
 
 #Calculate the projection matrix given corresponding 2D and 3D points
 
-
+'''
 M_l = calculate_projection_matrix(Points_2D_l,Points_3D_l)
 M_r = calculate_projection_matrix(Points_2D_r,Points_3D_r)
 
@@ -22,24 +22,7 @@ M_r = calculate_projection_matrix(Points_2D_r,Points_3D_r)
 [Projected_2D_Pts_l, Residual_l] = evaluate_points( M_l, Points_2D_l, Points_3D_l)
 [Projected_2D_Pts_r, Residual_r] = evaluate_points( M_r, Points_2D_r, Points_3D_r)
 
-
-
-# visualize points
-visualize_points(Points_2D_l,Projected_2D_Pts_l)
-visualize_points(Points_2D_r,Projected_2D_Pts_r)
-
-
-#Calculate the camera center using the M found from previous step
-Center_l = compute_camera_center(M_l)
-Center_r = compute_camera_center(M_r)
-
-
-#compute the fundamental matrix
-
-# F=
-# F, mask = cv2.findFundamentalMat(img1_points, img2_points, cv2.FM_RANSAC)
-F=cv2.stereoCalibrate(objpoints, imgpoints_r,imgpoints_l)
-
+'''
 ####################################################################################
 
 import cv2
@@ -117,3 +100,21 @@ print("rvecs : \n")
 print(rvecs)
 print("tvecs : \n")
 print(tvecs)
+
+
+#compute the fundamental matrix
+
+# F=
+# F, mask = cv2.findFundamentalMat(img1_points, img2_points, cv2.FM_RANSAC)
+F=cv2.stereoCalibrate(objpoints, imgpoints_r,imgpoints_l)
+
+# visualize points
+visualize_points(Points_2D_l,Projected_2D_Pts_l)
+visualize_points(Points_2D_r,Projected_2D_Pts_r)
+
+
+#Calculate the camera center using the M found from previous step
+Center_l = compute_camera_center(M_l)
+Center_r = compute_camera_center(M_r)
+
+
