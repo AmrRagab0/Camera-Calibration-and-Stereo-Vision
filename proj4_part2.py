2ca8f99331005e511b093f1b98828d6b096a2534
AmrRagab0 <48718803+AmrRagab0@users.noreply.github.com>
2022-05-14 15:47:25 +0200

    Update proj4_part2.py

--- proj4_part2.py
+++ proj4_part2.py
@@ -26,6 +26,7 @@ def camera_calibration(images_dir):
 
     # Defining the dimensions of checkerboard
     CHECKERBOARD = (7,7)
+    frame_size = (960,1280)
     criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
     # Creating vector to store vectors of 3D points for each checkerboard image
@@ -40,80 +41,49 @@ def camera_calibration(images_dir):
     prev_img_shape = None
 
     # Extracting path of individual image stored in a given directory
-    # images = glob.glob('./images/*.jpg')
+    
+    #images = glob.glob(images_dir+'/*.jpeg')
     images = glob.glob(images_dir+'/*.jpeg')
-    print(len(images))
-    print("path---- ",images_dir )
+    
     gray=[]
     for fname in images:
         img = cv2.imread(fname)
         gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-        #cv2.imshow('gray',gray)
+
         # Find the chess board corners
         # If desired number of corners are found in the image then ret = true
         ret, corners = cv2.findChessboardCorners(gray, CHECKERBOARD, cv2.CALIB_CB_ADAPTIVE_THRESH+cv2.CALIB_CB_FAST_CHECK+cv2.CALIB_CB_NORMALIZE_IMAGE)
-        corners = np.array([corner for [corner] in corners])
+        #corners = np.array([corner for [corner] in corners])
 
-        #print("ret\n",ret)
-        #print("corners\n",corners)
-
-        """
-        If desired number of corner are detected,
-        we refine the pixel coordinates and display 
-        them on the images of checker board
-        """
+     
         if ret == True:
 
-            print("------- in ret=true")
+            #print("------- in ret=true",objp)
             objpoints.append(objp)
             # refining pixel coordinates for given 2d points.
             corners2 = cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
 
             imgpoints.append(corners2)
-            print("---if-imgpoints------ ", np.shape(imgpoints))
+            #print("---if-imgpoints------ ", np.shape(imgpoints))
 
             # Draw and display the corners
             img = cv2.drawChessboardCorners(img, CHECKERBOARD, corners2,ret)
 
-        break
+        
         #cv2.imshow('img',img)
         #cv2.waitKey(0)
 
     cv2.destroyAllWindows()
 
     #h,w = img.shape[:2]
-
-    """
-    Performing camera calibration by 
-    passing the value of known 3D points (objpoints)
-    and corresponding pixel coordinates of the 
-    detected corners (imgpoints)
-    """
-
-    
-    print("----gray------ ", gray.shape)
-    '''
-    print("----imgpoints------ ", np.shape(imgpoints))
-    print("----objpoints------ ", np.shape(objpoints))
-    print("---------objpoints------ ",objpoints)
-    '''
-    print("---------imgpoints------ ",imgpoints)
-    print("----imgpoints------ ", np.shape(imgpoints))
-
-    imgpoints=np.array(imgpoints)
-    imgpoints=np.reshape(imgpoints,(49,2))
-
-    objpoints=np.array(objpoints)
-    objpoints=np.reshape(objpoints,(49,3))
-    print("----imgpoints------ ", np.shape(imgpoints))
-
+  
 
 #    ret, K, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1],None,None)
     #ret, K, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape(),None,None)
 
     ret, K, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, np.shape(gray)[::-1],None,None)
-
-    
+    #ret, K, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, np.shape(gray)[::-1],None,None)
+    print(np.shape(gray)[::-1])
     
     R = cv2.Rodrigues(rvecs[0])[0]
     t = tvecs[0]
@@ -129,31 +99,36 @@ P = np.matmul(mtx,Rt) # A[R|t]
     '''
     
 
-    return M,objpoints, imgpoints,K
+    return M,objpoints, imgpoints,K,dist
 
 
 
 
-data_dir = os.path.dirname(__file__) + '../data/'
+data_dir = os.path.dirname(__file__) #+ '../data/'
 
-images_dir_r=data_dir+'/right'
-images_dir_l=data_dir+'/left'
+from pathlib import Path
+data_dir = str(Path(data_dir).parents[0])
+
+images_dir_r=data_dir+'/data/right'
+images_dir_l=data_dir+'/data/left'
 
 #Validate the computed projection matrix as done in PART I
 
-M_r,objpoints_r, imgpoints_r,K_dash=camera_calibration(images_dir_r)
-M_l,objpoints_l, imgpoints_l,K=camera_calibration(images_dir_l)
+
+M_r,objpoints_r, imgpoints_r,K_dash,dist_r=camera_calibration(images_dir_r)
+M_l,objpoints_l, imgpoints_l,K,dist_l=camera_calibration(images_dir_l)
 
 
-print("objpoints_r--------",len(objpoints_r))
-print("imgpoints_r--------",len(imgpoints_r))
+print("objpoints_r--------",np.shape(objpoints_r))
+print("imgpoints_r--------",np.shape(imgpoints_r))
 
-M_r_0=calculate_projection_matrix(imgpoints_r,objpoints_r)
-M_l_0=calculate_projection_matrix(imgpoints_l,objpoints_l)
+#M_r_0=calculate_projection_matrix(imgpoints_r,objpoints_r)
+#M_l_0=calculate_projection_matrix(imgpoints_l,objpoints_l)
 
-print("the difference between calibrated and implemented  right projection matrix M_r= ",M_r-M_r_0)
-print("the difference between calibrated and implemented  left projection matrix M_l= ",M_l-M_l_0)
-
+#print("the difference between calibrated and implemented  right projection matrix M_r= ",M_r-M_r_0)
+#print("the difference between calibrated and implemented  left projection matrix M_l= ",M_l-M_l_0)
+# will evaluate M later
+'''
 # evaluate points
 [Projected_2D_Pts_l, Residual_l] = evaluate_points( M_l, imgpoints_l,objpoints_l) 
 [Projected_2D_Pts_r, Residual_r] = evaluate_points( M_r, imgpoints_r,objpoints_r)
@@ -161,12 +136,15 @@ print("the difference between calibrated and implemented  left projection matrix
 #Calculate the camera center using the M found from previous step
 Center_l = compute_camera_center(M_l)
 Center_r = compute_camera_center(M_r)
-
+'''
 #compute the fundamental matrix
 # F=
 # F, mask = cv2.findFundamentalMat(img1_points, img2_points, cv2.FM_RANSAC)
-S=cv2.stereoCalibrate(objpoints_r, imgpoints_r,imgpoints_l)    # objpoints_r may cause errors ?
-E=S[:-2]
+
+#flags = 0
+#flags |= cv.CALIB_FIX_INTRINSIC
+retStereo, newCameraMatrixL, distL, newCameraMatrixR, distR, rot, trans, essentialMatrix, fundamentalMatrix = cv2.stereoCalibrate(objpoints_r, imgpoints_l, imgpoints_r, M_l,dist_l,M_r,dist_r,(960,1280))    # objpoints_r may cause errors ?
+E=essentialMatrix[:-2]
 
 
 # print("Camera matrix : \n")
